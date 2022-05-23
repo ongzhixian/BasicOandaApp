6b250e2310b9af750a63cfbbf56498d1eaf9fac4
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-05-23 13:43:37 +0800

    add FileSystemHelper

---
+++ BasicOandaApp.ConsoleApp/Helpers/FileSystemHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace BasicOandaApp.ConsoleApp.Helpers;
+
+internal static class FileSystemHelper
+{
+    /// <summary>
+    /// Get full path; creates directory if it does not exists
+    /// </summary>
+    /// <param name="outputDirectoryPath"></param>
+    /// <returns></returns>
+    internal static string GetEnsuredFullPath(string outputDirectoryPath)
+    {
+        var outputDirectoryFullPath = Path.GetFullPath(outputDirectoryPath);
+
+        if (!Directory.Exists(outputDirectoryFullPath))
+        {
+            Directory.CreateDirectory(outputDirectoryFullPath);
+        }
+
+        return outputDirectoryFullPath;
+    }
+}
